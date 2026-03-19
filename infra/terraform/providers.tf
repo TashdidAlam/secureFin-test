9ededf9c67b748aa1364ee4ff024a4ff0a02fc16
TashdidAlam <tashdidnazib@gmail.com>
2026-03-19 16:29:28 +0600

    fix: skip provider registration to avoid deprecated namespace errors
    AzureRM auto-registers all resource providers, but Microsoft.Media,
    Microsoft.TimeSeriesInsights, and Microsoft.MixedReality are deprecated
    and return 404. skip_provider_registration = true bypasses this.

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -9,8 +9,9 @@
 provider "azurerm" {
   features {}
 
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  client_id       = var.client_id
-  use_oidc        = true
+  subscription_id            = var.subscription_id
+  tenant_id                  = var.tenant_id
+  client_id                  = var.client_id
+  use_oidc                   = true
+  skip_provider_registration = true
 }
