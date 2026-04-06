dd27480501ceca8a1a9e3754afc2999195d5cc6c
TashdidAlam <tashdidnazib@gmail.com>
2026-04-06 09:53:53 +0600

    vm group changed

--- infra/terraform/modules/aks/variables.tf
+++ infra/terraform/modules/aks/variables.tf
@@ -106,7 +106,7 @@ variable "system_node_count" {
 variable "system_node_vm_size" {
   description = "VM size for system nodes (burstable is fine — system pods are lightweight)"
   type        = string
-  default     = "Standard_B2s_v2"
+  default     = "Standard_D2s_v3"
 }
 
 # -----------------------------------------------------------------------------
